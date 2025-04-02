6938b22da982d74fdf3813980841a77051ae47d3
samuel <samuelsotointernational@gmail.com>
2025-04-02 02:16:52 -0600

    Worked on the fetch request for a random word

--- client/index.js
+++ client/index.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const playButton = document.getElementById("play-button");
     const exitButton = document.getElementById("exit-button");
+    const secretWordElement = document.getElementById("secret-word");
+    const guess = document.getElementById("guess");
+    const word = randomWord();
 
     if (playButton) {
         playButton.addEventListener("click", () => {
@@ -12,5 +15,18 @@ document.addEventListener("DOMContentLoaded", () => {
         exitButton.addEventListener("click", () => {
             history.back();
         });
+    }
+
+    secretWordElement.textContent = `${secretWordElement.textContent} ${word}`;
+
+    async function randomWord() {
+        var check = true;
+        while (check) {
+            const response = await fetch("https://random-word-api.vercel.app/api?words=1")
+            const word = response.json;            
+            if (word != null) {
+                check = false;
+            }
+        }
     }
 });
